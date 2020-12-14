f0464cd5516ade52d5e56a5a143b97fc7bcca384
rifqisbgj <rifqi.subagja4@gmail.com>
2020-12-14 16:15:26 +0700

    update save event

--- app/Http/Controllers/Api/SimpankajianController.php
+++ app/Http/Controllers/Api/SimpankajianController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Kajian;
 use App\Simpankajian;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,7 +13,9 @@ class SimpankajianController extends Controller
     public function simpankajian(Request $req)
     {
         $simpan = Simpankajian::where('id_kajian',$req->id_kajian)->where('id_user',Auth::user()->id)->get();
+        $a = Kajian::where('id',$req->id_kajian)->first();
         if(count($simpan)>0){
+            $a->suka_kajian -= 1;
             $simpan[0]->delete();
             return response()->json([
                 'success' => true,
@@ -23,6 +26,7 @@ class SimpankajianController extends Controller
         $simpan = new Simpankajian;
         $simpan->id_kajian = $req->id_kajian;
         $simpan->id_user = Auth::user()->id;
+        $a->suka_kajian += 1;
         $simpan->tambahsimpan($req->id_kajian);
         $simpan->save();
 
